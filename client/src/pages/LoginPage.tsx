01f22b73a10394f4a48f8bcea9f3a5a131a3b55f
AlwaysaqiooOfficial <aqioputra746@gmail.com>
2026-09-09 19:38:50 +0700

    Update LoginPage.tsx

--- client/src/pages/LoginPage.tsx
+++ client/src/pages/LoginPage.tsx
@@ -60,7 +60,7 @@ export default function LoginPage() {
               value={identifier}
               onChange={(e) => setIdentifier(e.target.value)}
               className="w-full rounded-lg border border-slate-300 px-3 py-2 text-sm outline-none transition focus:border-brand-500 focus:ring-2 focus:ring-brand-100"
-              placeholder="guru.budi"
+              placeholder=""
               autoComplete="username"
             />
           </div>}
